7aea0306f0d2a0a4a18b2a08c907c69e1aeae939
MochaMics <Mico.o.Ramos@gmail.com>
2022-11-26 17:25:07 -0800

    Lesson B

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private const float spawnRate = 2.0f;
     public List<GameObject> prefabs;
 
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 
     IEnumerator SpawnTarget()
@@ -21,5 +25,11 @@ public class GameManager : MonoBehaviour
             Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
         }
 
+    }
+
+    public void UpdateScore(int scoreDelta)
+    {
+        score += scoreDelta;
+        scoreText.text = "Score: " + score;
     }
 }
